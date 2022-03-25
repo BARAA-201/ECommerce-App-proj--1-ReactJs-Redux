0f82bf71becdaf5f6f89138fa4cf6c400cb68e85
ALBARAA ABDO <99486748+BARAA-201@users.noreply.github.com>
2022-03-25 07:52:34 +0300

    added a background image and organize the content

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './home.css';
 
-export default function() {
+export default function Home() {
   return (
-    <div className='home'>
-            <p className="card-text"><h1>Welcome to BENGO where everything you need is available.
-            </h1>
-              Browse our categories for more specific items, 
-            <br></br>Add to your cart and keep browsing.
-            <br></br>At any time you can visit the cart to modify your shopping items and proceed to the checkout.</p>
-            <div className='visit'>Now visit the products section from the product menu in the top.</div>
+    <div class="card bg-dark text-white border-0" >
+  <img src="assets/banar1.jpg" class="card-img" alt="Background" id="back1"/>
+  <div class="card-img-overlay">
+    <h1 class="card-title" id="homeTitle">Welcome to BENGO, where <br></br>
+    <span id="everything">everything you need is</span><br></br> 
+    <span id="available">available</span></h1>
+    <p class="card-text" id="homeP">Browse our categories for more specific <br></br>
+    items, and add to your cart and keep browsing.
+    <br></br>At any time you can visit the cart to <br></br>
+    modify your shopping items and
+    <br></br>proceed to the checkout.</p>
+    <div className='visit'>Now, take a look at the<br></br>
+    products section<br></br>
     </div>
+  </div>
+</div>
   )
 }
